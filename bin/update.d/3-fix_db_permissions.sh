3a0fe0899b9723422918928f559326dd44735bb2
David Waring <dev@davidwaring.net>
2020-10-05 13:35:13 -0400

    Update: fix DB permissions

---
+++ bin/update.d/3-fix_db_permissions.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+#
+# FIX DATABASE PERMISSIONS
+# This script will fix various database permissions for the web_usr user
+#
+
+BB_HOME="$1"
+BREEDBASE="$BB_HOME/bin/breedbase"
+DOCKER_COMPOSE_FILE="$BB_HOME/docker-compose.yml"
+
+# SQL to fix Database Permissions
+SQL_URL="https://raw.githubusercontent.com/TriticeaeToolbox/loading-scripts/master/sql/web_usr_grants.sql"
+SQL=$(curl -s "$SQL_URL")
+
+# Docker compose location
+DOCKER_COMPOSE=$(which docker-compose)
+DOCKER_DB_SERVICE="breedbase_db"
+
+# PSQL Location
+PSQL=$(which psql)
+
+# Get the defined web services
+mapfile -t services <<< $("$DOCKER_COMPOSE" -f "$DOCKER_COMPOSE_FILE" config --services)
+
+
+echo "==> Setting Database Permissions..."
+
+# Get postgres password from user
+read -sp "Postgres password: " postgres_pass
+echo ""
+
+# Process each web instance
+for service in "${services[@]}"; do
+   if [[ "$service" != "$DOCKER_DB_SERVICE" ]]; then
+        echo "... fixing $service database"
+        PGPASSWORD="$postgres_pass" psql -h localhost -U postgres -d "cxgn_$service" -c "$SQL"
+        if [ $? -ne 0 ]; then exit 1; fi
+    fi
+done
